fb941cb8e027f122d996952e14cd5a73a7c07cef
ranagargawi <ranagargawi@icloud.com>
2024-11-28 14:04:17 +0200

    started doing the canny filter. did the padding and the gaussian

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,8 @@
 #define RED 0.299
 #define GREEN 0.587
 #define BLUE 0.114
+unsigned char *canny(unsigned char *buffer, int width, int height, float scale, float lower, float upper);
+float *convolution(float *buffer, float *newBuffer, int width, int height, float *kernel, int kwidth, int kheight, float norm);
 
 unsigned char *greyScale(unsigned char *input, int length, float red, float green, float blue);
 int main(int argc, char *argv[])
@@ -48,3 +50,65 @@ unsigned char *greyScale(unsigned char *input, int length, float red, float gree
     return result;
 }
 //
+unsigned char *canny(unsigned char *buffer, int width, int height, float scale, float lower, float upper)
+{
+    float gaussian[] = {1, 2, 1, 2, 4, 2, 1, 2, 1};
+    int kernelHeight = 3;
+    int kernelWidth = 3;
+    int h = (kernelHeight - 1) / 2;
+    int w = (kernelWidth - 1) / 2;
+    float *blurred = new float[width * height];
+
+    float *padded = new float[width * height];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            padded[j + i * width] = buffer[j + i * width];
+        }
+    }
+
+    convolution(padded, blurred, width, height, gaussian, kernelWidth, kernelHeight, 1.0 / 16);
+}
+float *convolution(float *buffer, float *padded, int width, int height, float *kernel, int kernelWidth, int kernelHeight, float norm)
+{
+    for (int i = 0; i < kernelHeight; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            padded[j + i * width] = buffer[j + i * width];
+            padded[j + (height - 1 - i) * width] = buffer[j + (height - 1 - i) * width];
+        }
+    }
+    for (int i = kernelHeight; i < height; i++)
+    {
+        for (int j = 0; j < kernelWidth; j++)
+        {
+            padded[j + i * width] = buffer[j + i * width];
+            padded[width - 1 - j + i * width] = buffer[width - 1 - j + i * width];
+        }
+    }
+
+    for (int i = (kernelHeight - 1) / 2; i < height - (kernelHeight - 1) / 2; i++)
+    {
+        for (int j = (kernelWidth - 1) / 2; j < width - (kernelHeight - 1) / 2; j++)
+        {
+            applyKernel(buffer, padded, width, j, i, kernel, kernelWidth, kernelHeight, norm);
+        }
+    }
+
+    return padded;
+}
+
+void applyKernel(float *buffer, float *padded, int width, int x, int y, float *kernel, int kernelWidth, int kernelHeight, float norm)
+{
+    float avg = 0;
+    for (int i = 0; i < kernelHeight; i++)
+    {
+        for (int j = 0; j < kernelWidth; j++)
+        {
+            avg += buffer[x - (kernelWidth - 1) / 2 + j + (y - (kernelHeight - 1) / 2 + i) * width] * kernel[j + (i)*kernelWidth];
+        }
+    }
+    padded[x + y * width] = avg * norm;
+}
