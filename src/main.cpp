ed72e398de0f4b727bf3a7647ffa1da2ab0f0ace
ranagargawi <ranagargawi@icloud.com>
2024-11-29 16:12:46 +0200

    corrected canny

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,64 @@
+// if you wish to run with makefile extantion, try removinf the 2 defines below.
+
+// #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 
+// #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb/stb_image_write.h>
 #include <cmath>
 #include <iostream>
-#include <algorithm>
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+// gray scale weights
+#define RED_WEIGHT 0.2989
+#define GREEN_WEIGHT 0.5870
+#define BLUE_WEIGHT 0.1140
 
-#include <Debugger.h>
-#include <VertexBuffer.h>
-#include <VertexBufferLayout.h>
-#include <IndexBuffer.h>
-#include <VertexArray.h>
-#include <Shader.h>
-#include <Texture.h>
-#include <Camera.h>
-
-#include <iostream>
-#define RED 0.299
-#define GREEN 0.587
-#define BLUE 0.114
-#define BLACK 255
-#define WHITE 0
+// canny constants
+#define WHITE 255
+#define BLACK 0
 #define NON_RELEVANT 0
-#define T_WEAK 1
-#define T_STRONG 2
+#define WEAK 1
+#define STRONG 2
 #define CANNY 0.25
 #define M_PI 3.14159265358979323846
-#define COMPRESSED 16
-// compressed grayscale
+
+// Floyed-Steinberg dithering
+#define COMPRESSED 16 // compressed grayscale
 #define ALPHA 7 / 16.0
 #define BETA 3 / 16.0
 #define GAMMA 5 / 16.0
 #define DELTA 1 / 16.0
-unsigned char *fsErrorDiffDithering(unsigned char *buffer, int width, int height, float a, float b, float c, float d);
-unsigned char trunc(unsigned char p);
-unsigned char *canny(unsigned char *buffer, int width, int height, float scale, float lower, float upper);
-int doubleThreshhlding(unsigned char pixel, int low, int high);
 
+unsigned char *greyScale(unsigned char *buffer, int length, float gw, float rw, float bw);
+unsigned char *canny(unsigned char *buffer, int width, int height, float scale, float lower, float upper);
 float *convolution(float *buffer, float *newBuffer, int width, int height, float *kernel, int kwidth, int kheight, float norm);
-void applyKernel(float *buffer, float *padded, int width, int x, int y, float *kernel, int kernelWidth, int kernelHeight, float norm);
-float pixelClip(float pixel);
-unsigned char *greyScale(unsigned char *input, int length, float red, float green, float blue);
-unsigned char *halftone(unsigned char *buffer, int width, int height);
-int main(int argc, char *argv[])
+void applyKernel(float *buffer, float *newBuffer, int width, int x, int y, float *kernel, int kwidth, int kheight, float norm);
+float clippedPixel(float p);
+int doubleThreshhldingPixel(unsigned char p, int lower, int upper);
+
+unsigned char *floyedSteinberg(unsigned char *buffer, int width, int height, float a, float b, float c, float d);
+unsigned char trunc(unsigned char p);
+int main(void)
 {
     std::string filepath = "res/textures/Lenna.png";
     int width, height, comps;
     int req_comps = 4;
-    unsigned char *buffer = stbi_load(filepath.c_str(), &width, &height, &comps, req_comps);
+    unsigned char *original = stbi_load(filepath.c_str(), &width, &height, &comps, req_comps);
 
-    unsigned char *greyBuffer = greyScale(buffer, width * height, RED, GREEN, BLUE);
+    unsigned char *greyBuffer = greyScale(original, width * height, RED_WEIGHT, GREEN_WEIGHT, BLUE_WEIGHT);
     int result = stbi_write_png("res/textures/Grayscale.png", width, height, 1, greyBuffer, width);
+
     unsigned char *cannyBuffer = canny(greyBuffer, width, height, CANNY, 0.1, 0.15);
     result = result + stbi_write_png("res/textures/Canny.png", width, height, 1, cannyBuffer, width);
-    unsigned char *fsBuffer = fsErrorDiffDithering(greyBuffer, width, height, ALPHA, BETA, GAMMA, DELTA);
-    result += stbi_write_png("res/textures/FloyedSteinberg.png", width, height, 1, fsBuffer, width);
-    unsigned char *halftoneBuff = halftone(greyBuffer, width, height);
-    result += stbi_write_png("res/textures/Halftone.png", width * 2, height * 2, 1, halftoneBuff, width * 2);
 
+    unsigned char *fsBuffer = floyedSteinberg(greyBuffer, width, height, ALPHA, BETA, GAMMA, DELTA);
+    result += stbi_write_png("res/textures/FloyedSteinberg.png", width, height, 1, fsBuffer, width);
     std::cout << result << std::endl;
-    delete[] buffer;
+
+    delete[] original;
     delete[] greyBuffer;
     delete[] cannyBuffer;
-    delete[] halftoneBuff;
+    // delete[] halfBuff;
     delete fsBuffer;
     return 0;
 }
@@ -86,17 +78,17 @@ unsigned char *canny(unsigned char *buffer, int width, int height, float scale, 
     float ySobel[] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
     float gaussian[] = {1, 2, 1, 2, 4, 2, 1, 2, 1};
 
-    int kerelheight = 3;
-    int kernelwidth = 3;
-    int h = (kerelheight - 1) / 2;
-    int w = (kernelwidth - 1) / 2;
-    int *strenght = new int[width * height];
+    int kernelheight = 3;
+    int kerenlwidth = 3;
+    int h = (kernelheight - 1) / 2;
+    int w = (kerenlwidth - 1) / 2;
+    int *strength = new int[width * height];
     float *angles = new float[width * height];
-    float *blurredImage = new float[width * height];
+    float *blurred = new float[width * height];
     float *xConv = new float[width * height];
     float *yConv = new float[width * height];
-    unsigned char *gradients = new unsigned char[width * height];
-    unsigned char *outlines = new unsigned char[width * height];
+    unsigned char *gradient = new unsigned char[width * height];
+    unsigned char *outlined = new unsigned char[width * height];
     float currAngel = 0;
     int vecXSign = 0;
     int vecYSign = 0;
@@ -113,22 +105,22 @@ unsigned char *canny(unsigned char *buffer, int width, int height, float scale, 
         }
     }
     // reducing noise
-    convolution(fBuffer, blurredImage, width, height, gaussian, kernelwidth, kerelheight, 1.0 / 16);
+    convolution(fBuffer, blurred, width, height, gaussian, kerenlwidth, kernelheight, 1.0 / 16);
 
-    // finding gradient and angles..
+    // finding gradient and angels
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             if (i < h || i > height - 1 - h || j < w || j > width - 1 - w)
             {
-                gradients[j + i * width] = BLACK;
+                gradient[j + i * width] = BLACK;
                 angles[j + i * width] = BLACK;
                 continue;
             }
-            applyKernel(blurredImage, xConv, width, j, i, xSobel, kernelwidth, kerelheight, scale);
-            applyKernel(blurredImage, yConv, width, j, i, ySobel, kernelwidth, kerelheight, scale);
-            gradients[i * width + j] = pixelClip(std::sqrt(xConv[i * width + j] * xConv[i * width + j] + yConv[i * width + j] * yConv[i * width + j]));
+            applyKernel(blurred, xConv, width, j, i, xSobel, kerenlwidth, kernelheight, scale);
+            applyKernel(blurred, yConv, width, j, i, ySobel, kerenlwidth, kernelheight, scale);
+            gradient[i * width + j] = clippedPixel(std::sqrt(xConv[i * width + j] * xConv[i * width + j] + yConv[i * width + j] * yConv[i * width + j]));
             angles[j + i * width] = std::atan2(yConv[j + i * width], xConv[j + i * width]) * (180 / M_PI);
         }
     }
@@ -140,7 +132,7 @@ unsigned char *canny(unsigned char *buffer, int width, int height, float scale, 
         {
             if (i < h || i > height - 1 - h || j < w || j > width - 1 - w)
             {
-                outlines[j + i * width] = BLACK;
+                outlined[j + i * width] = BLACK;
                 continue;
             }
             currAngel = angles[j + i * width] < 0 ? angles[j + i * width] + 360 : angles[j + i * width];
@@ -170,71 +162,73 @@ unsigned char *canny(unsigned char *buffer, int width, int height, float scale, 
                 vecYSign = 1;
             }
 
-            currPixel = gradients[j + i * width];
-            posPixel = gradients[j + vecXSign + (i + vecYSign) * width];
-            negPixel = gradients[j - vecXSign + (i - vecYSign) * width];
+            currPixel = gradient[j + i * width];
+            posPixel = gradient[j + vecXSign + (i + vecYSign) * width];
+            negPixel = gradient[j - vecXSign + (i - vecYSign) * width];
 
             if (posPixel < currPixel && negPixel < currPixel)
             {
-                outlines[j + (i)*width] = currPixel;
+                outlined[j + (i)*width] = currPixel;
             }
             else
             {
-                outlines[j + i * width] = BLACK;
+                outlined[j + i * width] = BLACK;
             }
 
             // Double threashholding
-            strenght[j + i * width] = doubleThreshhlding(outlines[j + i * width], lower * WHITE, upper * WHITE);
+            strength[j + i * width] = doubleThreshhldingPixel(outlined[j + i * width], lower * WHITE, upper * WHITE);
         }
-    } // Hysteresis
+    }
+
+    // Hysteresis
     for (int i = h; i < height - h; i++)
     {
         for (int j = w; j < width - w; j++)
         {
-            if (strenght[j + i * width] == NON_RELEVANT)
-                outlines[j + i * width] = BLACK;
-            if (strenght[j + i * width] == T_STRONG)
-                outlines[j + i * width] = WHITE;
-            if (strenght[j + i * width] == T_WEAK)
+            if (strength[j + i * width] == NON_RELEVANT)
+                outlined[j + i * width] = BLACK;
+            if (strength[j + i * width] == STRONG)
+                outlined[j + i * width] = WHITE;
+            if (strength[j + i * width] == WEAK)
             {
-                if (strenght[j - 1 + (i - 1) * width] == T_STRONG ||
-                    strenght[j + (i - 1) * width] == T_STRONG ||
-                    strenght[j + 1 + (i - 1) * width] == T_STRONG ||
-                    strenght[j - 1 + (i)*width] == T_STRONG ||
-                    strenght[j + 1 + (i)*width] == T_STRONG ||
-                    strenght[j - 1 + (i + 1) * width] == T_STRONG ||
-                    strenght[j + (i + 1) * width] == T_STRONG ||
-                    strenght[j + 1 + (i + 1) * width] == T_STRONG)
+                if (strength[j - 1 + (i - 1) * width] == STRONG ||
+                    strength[j + (i - 1) * width] == STRONG ||
+                    strength[j + 1 + (i - 1) * width] == STRONG ||
+                    strength[j - 1 + (i)*width] == STRONG ||
+                    strength[j + 1 + (i)*width] == STRONG ||
+                    strength[j - 1 + (i + 1) * width] == STRONG ||
+                    strength[j + (i + 1) * width] == STRONG ||
+                    strength[j + 1 + (i + 1) * width] == STRONG)
                 {
-                    outlines[j + i * width] = WHITE;
-                    strenght[j + i * width] = T_STRONG;
+                    outlined[j + i * width] = WHITE;
+                    strength[j + i * width] = STRONG;
                 }
                 else
-                    outlines[j + i * width] = BLACK;
+                    outlined[j + i * width] = BLACK;
             }
         }
     }
 
     delete[] xConv;
     delete[] yConv;
-    delete[] gradients;
-    delete[] strenght;
+    delete[] gradient;
+    delete[] strength;
     delete[] angles;
-    delete[] blurredImage;
-    return outlines;
-}
-int doubleThreshhlding(unsigned char p, int lower, int upper)
-{
-    if (p < lower)
-        return NON_RELEVANT;
-    else if (p >= lower && p <= upper)
-        return T_WEAK;
-    else
-        return T_STRONG;
+    delete[] blurred;
+    return outlined;
 }
 
-// working but only give convolution
-float *convolution(float *buffer, float *newBuffer, int width, int height, float *kernel, int kernelwidth, int kernelheight, float norm)
+int doubleThreshhldingPixel(unsigned char pixel, int lower, int upper)
+{
+    if (pixel < lower)
+        return NON_RELEVANT;
+    else if (pixel >= lower && pixel <= upper)
+        return WEAK;
+    else
+        return STRONG;
+}
+
+float *convolution(float *buffer, float *newBuffer, int width, int height, float *kernel, int kwidth, int kernelheight, float norm)
 {
     for (int i = 0; i < kernelheight; i++)
     {
@@ -246,7 +240,7 @@ float *convolution(float *buffer, float *newBuffer, int width, int height, float
     }
     for (int i = kernelheight; i < height; i++)
     {
-        for (int j = 0; j < kernelwidth; j++)
+        for (int j = 0; j < kwidth; j++)
         {
             newBuffer[j + i * width] = buffer[j + i * width];
             newBuffer[width - 1 - j + i * width] = buffer[width - 1 - j + i * width];
@@ -255,37 +249,37 @@ float *convolution(float *buffer, float *newBuffer, int width, int height, float
 
     for (int i = (kernelheight - 1) / 2; i < height - (kernelheight - 1) / 2; i++)
     {
-        for (int j = (kernelwidth - 1) / 2; j < width - (kernelheight - 1) / 2; j++)
+        for (int j = (kwidth - 1) / 2; j < width - (kernelheight - 1) / 2; j++)
         {
-            applyKernel(buffer, newBuffer, width, j, i, kernel, kernelwidth, kernelheight, norm);
+            applyKernel(buffer, newBuffer, width, j, i, kernel, kwidth, kernelheight, norm);
         }
     }
     return newBuffer;
 }
 
-void applyKernel(float *buffer, float *newBuffer, int width, int x, int y, float *kernel, int kwidth, int kheight, float norm)
+void applyKernel(float *buffer, float *newBuffer, int width, int x, int y, float *kernel, int kernelwidth, int kernelheight, float norm)
 {
-    float sum = 0;
-    for (int i = 0; i < kheight; i++)
+    float avg = 0;
+    for (int i = 0; i < kernelheight; i++)
     {
-        for (int j = 0; j < kwidth; j++)
+        for (int j = 0; j < kernelwidth; j++)
         {
-            sum += buffer[x - (kwidth - 1) / 2 + j + (y - (kheight - 1) / 2 + i) * width] * kernel[j + (i)*kwidth];
+            avg += buffer[x - (kernelwidth - 1) / 2 + j + (y - (kernelheight - 1) / 2 + i) * width] * kernel[j + (i)*kernelwidth];
         }
     }
-    newBuffer[x + y * width] = sum * norm;
+    newBuffer[x + y * width] = avg * norm;
 }
 
-float pixelClip(float p)
+float clippedPixel(float pixel)
 {
-    if (p > WHITE)
+    if (pixel > WHITE)
         return WHITE;
-    if (p < BLACK)
+    if (pixel < BLACK)
         return BLACK;
-    return p;
+    return pixel;
 }
 
-unsigned char *fsErrorDiffDithering(unsigned char *buffer, int width, int height, float a, float b, float c, float d)
+unsigned char *floyedSteinberg(unsigned char *buffer, int width, int height, float a, float b, float c, float d)
 {
     unsigned char *diffused = new unsigned char[width * height]; // copy of image with diffused error
     unsigned char *result = new unsigned char[width * height];
@@ -314,51 +308,4 @@ unsigned char trunc(unsigned char pixel)
 {
     // maps 0-255 into one of COMPRESSED_GS values spaced
     return (unsigned char)(pixel / 255.0 * COMPRESSED) / (float)COMPRESSED * 255.0;
-}
-
-unsigned char *halftone(unsigned char *buffer, int width, int height)
-{
-    int length = width * height;
-    unsigned char *result = new unsigned char[length * 4];
-    for (int i = 0; i < length; i++)
-    {
-        int row = i / width;
-        int col = i % width;
-        if (buffer[i] < WHITE / 5)
-        {
-            result[row * 2 * 2 * width + col * 2] = BLACK;
-            result[row * 2 * 2 * width + col * 2 + 1] = BLACK;
-            result[(row * 2 + 1) * 2 * width + col * 2] = BLACK;
-            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = BLACK;
-        }
-        else if (buffer[i] >= WHITE / 5 && buffer[i] < WHITE / 5 * 2)
-        {
-            result[row * 2 * 2 * width + col * 2] = BLACK;
-            result[row * 2 * 2 * width + col * 2 + 1] = BLACK;
-            result[(row * 2 + 1) * 2 * width + col * 2] = WHITE;
-            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = BLACK;
-        }
-        else if (buffer[i] >= WHITE / 5 * 2 && buffer[i] < WHITE / 5 * 3)
-        {
-            result[row * 2 * 2 * width + col * 2] = WHITE;
-            result[row * 2 * 2 * width + col * 2 + 1] = BLACK;
-            result[(row * 2 + 1) * 2 * width + col * 2] = WHITE;
-            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = BLACK;
-        }
-        else if (buffer[i] >= WHITE / 5 * 3 && buffer[i] < WHITE / 5 * 4)
-        {
-            result[row * 2 * 2 * width + col * 2] = BLACK;
-            result[row * 2 * 2 * width + col * 2 + 1] = WHITE;
-            result[(row * 2 + 1) * 2 * width + col * 2] = WHITE;
-            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = WHITE;
-        }
-        else if (buffer[i] >= WHITE / 5 * 4)
-        {
-            result[row * 2 * 2 * width + col * 2] = WHITE;
-            result[row * 2 * 2 * width + col * 2 + 1] = WHITE;
-            result[(row * 2 + 1) * 2 * width + col * 2] = WHITE;
-            result[(row * 2 + 1) * 2 * width + col * 2 + 1] = WHITE;
-        }
-    }
-    return result;
 }
